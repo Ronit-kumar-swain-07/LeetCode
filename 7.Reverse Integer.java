740ab2cc5a1b6dbdde2c580feab7638c9b621a61
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-11-16 12:10:20 +0530

    commit

--- 7.Reverse Integer.java
+++ 7.Reverse Integer.java
@@ -1,6 +1,6 @@
 /*
 7. Reverse Integer
-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the 
+Given Signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the 
 signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 Assume the Environment does not allow you to store 64-bit integers (signed or unsigned)
