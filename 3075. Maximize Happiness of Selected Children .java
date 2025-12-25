ab525a1cddfc01171f82f4b184384a17c33dd00d
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-12-25 15:22:40 +0530

    commit

--- 3075. Maximize Happiness of Selected Children .java
+++ 3075. Maximize Happiness of Selected Children .java
@@ -17,7 +17,7 @@ decreases by 1. Note that the happiness value cannot become negative and gets de
  * Output: 4
  * Explanation: We can pick 2 children in the following way:- 
 Pick the child with the happiness value == 3. The happiness value of the remaining children becomes [0,1].
- * - Pick the child with the happiness value == 1. The happiness value of the remaining child becomes [0]. 
+Pick the child with the happiness value == 1. The happiness value of the remaining child becomes [0]. 
 Note that the happiness value cannot become less than 0.
  * The sum of the happiness values of the selected children is 3 + 1 = 4.
 
