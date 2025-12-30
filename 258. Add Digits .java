4937318c2248ad84d4275ff34532c0b474d0c24d
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-12-30 21:08:38 +0530

    commit

--- 258. Add Digits .java
+++ 258. Add Digits .java
@@ -22,6 +22,22 @@ Constraints:
 
 //CODE
 
+/*
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+*/
+
 class Solution {
     public int addDigits(int num) {
         if(num == 0) {
